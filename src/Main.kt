5471c7d5b6d85fdde3d80ea08532adf321691cf7
anyonepaw <anyonepaw@anyonepaws-MacBook-Pro.local>
2019-01-18 00:19:22 +0300

    Initial commit

---
+++ src/Main.kt
@@ -0,0 +1,40 @@
+fun main() {
+    var a = ""
+    val b = ""
+
+    a = "Hello!"
+    //error // b = "Hello!";
+
+    var str: String
+
+    var d: Double
+
+    fun main() {
+
+        var str = ""
+
+        var i = 1
+
+        var d = 4.5
+
+    }
+
+
+    println("Hello world!")
+    println("I love " + "Kotlin")
+    println("Сумма " + 5 + " и " + 10 + " равна " + (5 + 10))
+
+
+    val i = 7
+    val j = 5
+    println(i*j)
+    println(i-j)
+
+    /* Использование интерполяции */
+    val a1 = 100
+    val b1 = 300
+    println("Разность $a1 и $b1 равна ${a1-b1}")
+    val name = "Alou"
+    println("My name is $name! Hello!")
+
+}
